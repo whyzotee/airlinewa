16b1fc87b7b2e93e7f07da8210b1368eb45dbfbf
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-02-22 11:10:48 +0700

    feat: client

---
+++ client/src/app/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import logo from "/logo.jpg";
+import preview from "/preview.webm";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  Drawer,
+  Input,
+} from "@heathmont/moon-core-tw";
+import HomeDropDown from "./components/dropdown";
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  // const handleClick = () => setIsOpen(true);
+
+  return (
+    <main>
+      <div className="fixed h-screen w-screen bg-black opacity-75"></div>
+      <video
+        className="fixed min-h-full min-w-full -z-10 bg-black object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={preview} type="video/mp4" />
+      </video>
+      <div className="flex flex-col h-screen justify-center items-center bg-transparent relative gap-4">
+        <div className="flex gap-4">
+          <Avatar className="rounded-xl" size="2xl" imageUrl={logo} />
+          <div className="text-white">
+            <h1 className="text-2xl">Fly with Airlinewa</h1>
+            <p>you never die alone</p>
+          </div>
+        </div>
+
+        {/* <Button
+          className="text-white border border-white rounded-lg cursor-pointer"
+          onClick={handleClick}
+        >
+          Fly Now, Book Here
+        </Button> */}
+        <Button className="text-white border border-white rounded-lg cursor-pointer">
+          Comming Soon
+        </Button>
+        <Drawer open={isOpen} setOpen={setIsOpen}>
+          <Drawer.Panel position="top" className="bg-white p-8 rounded-b-xl">
+            <div className="flex gap-4">
+              <HomeDropDown />
+              <HomeDropDown />
+              <Input
+                aria-label="default"
+                className="border-red-300 border rounded-lg w-56"
+              />
+              <Checkbox
+                label="เที่ยวบินเฉพาะแอร์ไลน์วา"
+                id="withLabel"
+                className=""
+              />
+            </div>
+          </Drawer.Panel>
+        </Drawer>
+      </div>
+    </main>
+  );
+}
+
+export default App;
